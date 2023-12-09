46a460be2da60420958a916a4351ccf424c9df5f
Morpheus3000 <morpheus.morphia@gmail.com>
2023-12-09 01:31:02 +0100

    Add Day 1 Trebuchet 2nd gold star.

--- day_1_trebuchet_part_two.py
+++ day_1_trebuchet_part_two.py
@@ -1,25 +1,68 @@
+def find_all_occurences_indices(main_string, tgt_string):
+    last_start = 0
+    ret = 0
+    all_indices = []
+    while ret != -1:
+        ret = main_string.find(tgt_string, last_start)
+        if ret != -1:
+            last_start = ret + 1
+            all_indices.append(ret)
+    all_indices.sort()
+
+    return all_indices
+
+
 def main(file_path):
 
     lines = [x.strip() for x in open(file_path, 'r').readlines()]
     print('[I] Found %d lines!' % len(lines))
     running_total = 0
-    valid_digits = ['one', 'two', 'three', 'four', 'five', 'six', 'seven',
-                    'eight', 'nine']
+    valid_digits = [['one', 1],
+                    ['two', 2],
+                    ['three', 3],
+                    ['four', 4],
+                    ['five', 5],
+                    ['six', 6],
+                    ['seven', 7],
+                    ['eight', 8],
+                    ['nine', 9],
+                    ['1', 1],
+                    ['2', 2],
+                    ['3', 3],
+                    ['4', 4],
+                    ['5', 5],
+                    ['6', 6],
+                    ['7', 7],
+                    ['8', 8],
+                    ['9', 9]]
+
+    valid_dict = {
+        x[0]: [] for x in valid_digits
+    }
+
+    # test_str = 'fourseven5seveneightsvtkcjdrfour'
+    # all_idx = find_all_occurences_indices(test_str, 'four')
+    # print(test_str)
+    # print(all_idx)
+
     for i, l in enumerate(lines):
         print('Processing line %d...' % i, end='')
-        cache = ''
-        num_list = []
-        for a in list(l):
-            if a.isnumeric():
-                num_list.append(int(a))
-            else:
-                cache += a
-                if cache in valid_digits:
-                    num_list.append(valid_digits.index(cache) + 1)
-                    cache = ''
-
+        first_idx = float('inf')
+        last_idx = -1
+        first_dig = -1
+        last_dig = -1
+        for n in valid_digits:
+            all_idx = find_all_occurences_indices(l, n[0])
+            if len(all_idx) > 0:
+                valid_dict[n[0]] = all_idx
+                if first_idx > all_idx[0]:
+                    first_idx = all_idx[0]
+                    first_dig = n[1]
+                if last_idx < all_idx[-1]:
+                    last_idx = all_idx[-1]
+                    last_dig = n[1]
         # Because it is always a 2 digit number
-        num = num_list[0] * 10 + num_list[-1]
+        num = first_dig * 10 + last_dig
         print('done!')
         print('\t[*] %s -> %d' % (l, num))
         running_total += num
@@ -28,5 +71,5 @@ def main(file_path):
 
 
 if __name__ == '__main__':
-    main(file_path='data\day1_part_two_test.str')
+    main(file_path='data\day1_part_two_input.str')
 
