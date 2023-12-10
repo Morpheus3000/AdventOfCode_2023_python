b86383a11002b99cda1c7b8014a7ed1d87b90814
Morpheus3000 <Morpheus.morphia@gmail.com>
2023-12-10 23:03:07 +0100

    Prep for day 5.

--- day_5/day_5.py
+++ day_5/day_5.py
@@ -1,6 +1,16 @@
 def main(file_path):
-    raise NotImplementedError
+    lines = [x.strip() for x in open(file_path, 'r').readlines()]
+    print('[I] Found %d lines!' % len(lines))
+    running_total = 0
+    for i, l in enumerate(lines):
+        print('Processing line %d...' % (i + 1), end='')
+
+        print('done!')
+        print('\t[*] %s -> %d' % (l, num))
+        running_total += num
+
+    print('[I] Final total is: ', running_total)
 
 
 if __name__ == '__main__':
-    main(file_path='../data/day1_input.str')
+    main(file_path='../data/day5_test.str')
