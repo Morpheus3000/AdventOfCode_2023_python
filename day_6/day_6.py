c4aba12fd639729f79e11f2a96e657e63a703226
Morpheus3000 <Morpheus.morphia@gmail.com>
2023-12-11 16:43:30 +0100

    Add day 6 solution.

--- day_6/day_6.py
+++ day_6/day_6.py
@@ -1,6 +1,31 @@
+import math
+from tqdm import tqdm
+
+
 def main(file_path):
-    raise NotImplementedError
+    lines = [x.strip() for x in open(file_path, 'r').readlines()]
+    print('[I] Found %d lines!' % len(lines))
+    running_total = 0
+    times = [int(x.strip()) for x in lines[0].split(':')[-1].strip().split(' ') if len(x.strip()) > 0]
+    dist = [int(x.strip()) for x in lines[1].split(':')[-1].strip().split(' ') if len(x.strip()) > 0]
+
+    win_prob = [0 for x in range(len(times))]
+    for i, t in tqdm(enumerate(times), total=len(times)):
+        tgt_dist = dist[i]
+        valid_hold = 0
+        for hold_t in tqdm(range(t), total=t, leave=False):
+            dist_trav = hold_t * (t - hold_t)
+            if dist_trav > tgt_dist:
+                valid_hold += 1
+        win_prob[i] = valid_hold
+
+    print(times)
+    print(dist)
+    print(win_prob)
+    running_total = math.prod(win_prob)
+    print('[I] Final total is: ', running_total)
 
 
 if __name__ == '__main__':
-    main(file_path='../data/day1_input.str')
+    # main(file_path='../data/day6_test.str')
+    main(file_path='../data/day6_input.str')
--- day_6/day_6_part_two.py
+++ day_6/day_6_part_two.py
@@ -1,6 +1,30 @@
+import math
+from tqdm import tqdm
+
+
 def main(file_path):
-    raise NotImplementedError
+    lines = [x.strip() for x in open(file_path, 'r').readlines()]
+    print('[I] Found %d lines!' % len(lines))
+    running_total = 0
+    times = int(''.join(
+        [x.strip() for x in lines[0].split(':')[-1].strip().split(' ') if
+         len(x.strip()) > 0]
+    ))
+    dist = int(''.join(
+        [x.strip() for x in lines[1].split(':')[-1].strip().split(' ') if
+         len(x.strip()) > 0]
+    ))
+
+    for hold_t in tqdm(range(times), total=times):
+        dist_trav = hold_t * (times - hold_t)
+        if dist_trav > dist:
+            running_total += 1
+
+    print('Total time: ', times)
+    print('Record distance: ', dist)
+    print('[I] Final total is: ', running_total)
 
 
 if __name__ == '__main__':
-    main(file_path='../data/day1_part_two_input.str')
+    main(file_path='../data/day6_input.str')
+
