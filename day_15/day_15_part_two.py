72a28d65d5ccecdfac30858b27c782e1d0446baf
Morpheus3000 <morpheus.morphia@gmail.com>
2023-12-16 04:06:00 +0100

    Add day 15 part 1 solution. Initialise part 2.

--- day_15/day_15_part_two.py
+++ day_15/day_15_part_two.py
@@ -1,6 +1,28 @@
+from tqdm import tqdm
+
+
+def load_data(data_path):
+
+    lines = [x.strip() for y in open(data_path, 'r').readlines() for x in
+             y.split(',')]
+    print('[I] Found %d lines!' % len(lines))
+    return lines
+
+
+def hash_algorithm(tgt_str):
+    cur_val = 0
+    elements = list(tgt_str)
+    for e in elements:
+        cur_val = ((cur_val + ord(e)) * 17) % 256
+    return cur_val
+
+
 def main(file_path):
-    raise NotImplementedError
+    data = load_data(file_path)
+    running_total = sum([hash_algorithm(x) for x in tqdm(data)])
+    print('[I] Final total is: ', running_total)
 
 
 if __name__ == '__main__':
-    main(file_path='../data/day1_part_two_input.str')
+    # main(file_path='../data/day15_test.str')
+    main(file_path='../data/day15_input.str')
